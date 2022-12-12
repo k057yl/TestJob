cf5dc004bd3d3c2d079690ea572d27f4601dc98e
k057yl <111869223+k057yl@users.noreply.github.com>
2022-12-12 02:36:28 +0200

    Update Player.cs

--- Player/Player.cs
+++ Player/Player.cs
@@ -6,37 +6,50 @@ public class Player : MonoBehaviour
     [SerializeField] internal Rigidbody rb;
     [SerializeField] internal Animator anim;
     [SerializeField] GameObject pizza;
+    [SerializeField] AudioSource[] id;
 
+    internal int coin;
+    internal int hp = 1;
 
     private void Start()
     {
-        pizza.SetActive(false);
-        
-    }
+        pizza.SetActive(false);       
+    }  
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Pizza")
-        {
-            pizza.SetActive(true);
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Target")
         {
+            id[2].Play();
             pizza.SetActive(false);
+        }
+        if (other.tag == "Pizza")
+        {
+            id[1].Play();
+            pizza.SetActive(true);
+            coin++;
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Traffic")
         {
+            id[0].Play();
             anim.SetBool("Crush", true);
-            Invoke("RestartLevel", 2f);
+            Invoke("GameOver", 1.8f);
+            hp--;
         }
+
     }
-    void RestartLevel()
+    public void GameOver()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ToMenu()
     {
         SceneManager.LoadScene(0);
     }
