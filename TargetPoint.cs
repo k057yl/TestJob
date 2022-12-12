0b8514fd3c0243be3567dc262b3e8669cad65004
k057yl <111869223+k057yl@users.noreply.github.com>
2022-12-12 02:39:57 +0200

    Update TargetPoint.cs

--- TargetPoint.cs
+++ TargetPoint.cs
@@ -1,14 +1,23 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TargetPoint : MonoBehaviour
 {
+    [SerializeField] GameObject can;
     [SerializeField] GameObject pizzaOpen;
+    [SerializeField] ParticleSystem win;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            Destroy(gameObject.GetComponent<Collider>());
+            GetComponent<Animator>().enabled= false;
+            win.Play();
             pizzaOpen.SetActive(true);
+            can.SetActive(true);
         }
     }
 }
